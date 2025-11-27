8d01cc6ddef9fad97987f059b543317cd0b2ce46
Pabloceda <pabloceda00@gmail.com>
2025-11-27 17:47:57 +0100

    Actualizacion ejerciios parte 2 27/11

---
+++ Ejercicio1.py
@@ -0,0 +1,6 @@
+def contador():
+    print("Los primeros 20 números naturales")   
+    for i in range(1, 21):
+        print(i)
+
+contador()
---
+++ Ejercicio2.py
@@ -0,0 +1,7 @@
+print("Programa numeros pares entre el 1 y el 200")
+i = 1
+while i <= 200:
+    if i % 2 == 0:
+        print(i)
+    i += 1  
+print("Fin del programa")
