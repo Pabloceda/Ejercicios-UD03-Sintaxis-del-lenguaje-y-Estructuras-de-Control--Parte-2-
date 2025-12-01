11efe123d8491b58079bfbe0305a75b7c95123d5
Pabloceda <pabloceda00@gmail.com>
2025-12-01 17:53:59 +0100

    update

--- Ejercicio10.py
+++ Ejercicio10.py
@@ -1,8 +1,10 @@
 def suma10primerosnum ():
-    i = 0
-    while i < 10:
-        sumanumeros = i + i
-        multiplicacion = i * i
+    i = 1
+    sumanumeros = 0
+    multiplicacion = 1
+    while i <= 10:
+        sumanumeros += i    
+        multiplicacion *= i
         i += 1
     print("La suma de los 10 primeros numeros es: ", sumanumeros)
     print("La multiplicacion de los 10 primeros numeros es: ", multiplicacion)
