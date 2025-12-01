e65ba724592a4b187e71892a3c5d04c8a22f2666
Pabloceda <pabloceda00@gmail.com>
2025-12-01 09:35:56 +0100

    update

--- Ejercicio19.py
+++ Ejercicio19.py
@@ -1,7 +1,7 @@
 def advinadordenumeros():
     print("--- Programa de adivinacion --- ")
     print("Piensa en un numero entre 1 y 100 y presiona Enter cuando estés listo.")
-    input()  # Esperar a que el usuario esté listo
+    input() 
 
     limite_inferior = 1
     limite_superior = 100
@@ -9,7 +9,6 @@ def advinadordenumeros():
     adivinado = False
 
     while not adivinado and limite_inferior <= limite_superior:
-        # Calculamos el punto medio
         intento = (limite_inferior + limite_superior) // 2
         intentos += 1
         
@@ -20,14 +19,12 @@ def advinadordenumeros():
             print(f"¡Genial! Tu numero es el {intento} Lo adiviné en {intentos} intentos.")
             adivinado = True
         elif respuesta == "mayor":
-            # Si el número del usuario es mayor, buscamos en la mitad superior
             limite_inferior = intento + 1
         elif respuesta == "menor":
-            # Si el número del usuario es menor, buscamos en la mitad inferior
             limite_superior = intento - 1
         else:
             print("Por favor, responde solo con 'mayor', 'menor' o 'igual'.")
-            intentos -= 1 # No contamos este intento si la entrada fue errónea
+            intentos -= 1
 
     if not adivinado:
         print("¡Vaya! Parece que has hecho trampas crack")
