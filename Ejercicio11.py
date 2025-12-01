e127615e60f6347a831ea10099ce33c54664a77a
Pabloceda <pabloceda00@gmail.com>
2025-12-01 20:28:12 +0100

    mod

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -2,3 +2,5 @@ altura = int(input("Introduce la altura de la escalera: "))
 
 for i in range(1, altura + 1):
     print("*" * i)
+
+
--- Ejercicio13.py
+++ Ejercicio13.py
@@ -1,8 +1,6 @@
 altura = int(input("Introduce la altura de la escalera: "))
 
 for i in range(1, altura + 1):
-
     for j in range(1, i + 1):
         print(j, end=" ") 
-
     print()
